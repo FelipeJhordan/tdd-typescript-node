c60c131d6f38efc30a0205aa14e9a20b29753cc7
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-14 15:38:45 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,16 +1,18 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
-const SALT = 12
-
+// Teve que mockar o metodo bcrypt para fazer passar o teste
+// usando um hash previsivel
+// o que importa aqui é testar se o metodo retorna algo
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
+    return await new Promise(resolve => resolve('hash'))
   }
 }))
 
+const salt = 12
 const makeSut = (): BcryptAdapter => {
-  return new BcryptAdapter(SALT)
+  return new BcryptAdapter(salt)
 }
 
 describe('Bcrypt Adapter', () => {
@@ -18,11 +20,21 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', SALT)
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
-  test('Should return a hash on success ', async () => {
+
+  test('Should return a hash on success', async () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throws Error if bcrypt throws error', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
