7f666d4cbaaac213c7f53e40cf1097f2a92efdd7
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-14 17:20:49 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,6 +10,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountColletion = MongoHelper.getColletion('accounts')
+    accountColletion.deleteMany({})
+  })
+
   const makeSut = () : AccountMongoRepository => {
     return new AccountMongoRepository()
   }
