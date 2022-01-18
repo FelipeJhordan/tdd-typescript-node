ffba3ccfd5ad196d63c1d69ce48f95c411ca0bd8
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-18 08:49:41 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,7 +1,7 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 import { ok, serverError } from '../../presentation/helpers/http/http-helper'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 
 const makeLogErrorRepository = (): LogErrorRepository => {
