7c2ab7c5ab84a98aa9d6d436de969466f7941634
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-18 13:56:18 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,15 +15,18 @@ describe('SignUp Routes', () => {
     const accountColletion = await MongoHelper.getColletion('accounts')
     await accountColletion.deleteMany({})
   })
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/v1/signup')
-      .send({
-        name: 'Felipe',
-        email: 'felipejhordan.alves@gmail.com',
-        password: 'samsung',
-        passwordConfirmation: 'samsung'
-      })
-      .expect(200)
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/v1/signup')
+        .send({
+          name: 'Felipe',
+          email: 'felipejhordan.alves@gmail.com',
+          password: 'samsung',
+          passwordConfirmation: 'samsung'
+        })
+        .expect(200)
+    })
   })
 })
