d8af24ada761d56d92b1e0ecdfc61b7c69b616d3
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-18 14:59:57 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -48,5 +48,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/v1/login')
+        .send({
+          email: 'felipejhordan.alves@gmail.com',
+          password: 'samsung'
+        })
+        .expect(401)
+    })
   })
 })
