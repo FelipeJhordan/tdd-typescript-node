a4ec6317fae679f8a83af2f3f535e8e821f8a384
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-17 21:22:25 -0300

    refactor: move Validation to protocols folder

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '../../errors'
-import { Validation } from './validation'
+import { Validation } from '../../protocols/validation'
 
 export class CompareFieldsValidation implements Validation {
   constructor (private fieldName: string, private fieldToCompareName: string) {}
