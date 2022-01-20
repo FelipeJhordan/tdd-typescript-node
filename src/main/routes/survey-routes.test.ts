b5f761826cba1d07ea8c92a83f29be17bccfbf90
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-20 11:04:06 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -40,43 +40,107 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
-  })
-  test('Should return 204 on add survey with valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Felipe',
-      email: 'felipejhordan.alves@gmail.com',
-      password: '123',
-      role: 'admin'
-    })
-    const id = MongoHelper.getObjectId(res.insertedId.toString())
-    const accessToken = sign({ id }, env.jwtSecret)
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-    await request(app)
-      .post('/api/v1/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [{
-          answer: 'Answer 1',
-          image: 'http://image-name.com'
-        },
-        {
-          answer: 'Answer 2'
-        }]
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Felipe',
+        email: 'felipejhordan.alves@gmail.com',
+        password: '123',
+        role: 'admin'
       })
-      .expect(204)
+      const id = MongoHelper.getObjectId(res.insertedId.toString())
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/v1/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
   })
+
   describe('GET /surveys', () => {
     test('Should return 403 on load surveys without accessToken', async () => {
       await request(app)
         .get('/api/v1/surveys')
         .expect(403)
+    })
+    test('Should return 204 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Felipe',
+        email: 'felipejhordan.alves@gmail.com',
+        password: '123'
+      })
+      const id = MongoHelper.getObjectId(res.insertedId.toString())
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .get('/api/v1/surveys')
+        .set('x-access-token', accessToken)
+        .expect(204)
+    })
+    test('Should return 200 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Felipe',
+        email: 'felipejhordan.alves@gmail.com',
+        password: '123'
+      })
+      const id = MongoHelper.getObjectId(res.insertedId.toString())
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [{
+            image: 'other_image',
+            answer: 'other_answer'
+          }, {
+            answer: 'trash_answer'
+          }],
+          date: new Date()
+        }
+      ])
+      await request(app)
+        .get('/api/v1/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
