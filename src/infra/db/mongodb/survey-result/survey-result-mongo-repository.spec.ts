30a60ef44a9a4b7cfb2667b67a07173efc8f1ebd
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-20 20:52:08 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,6 +1,8 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { Collection } from 'mongodb'
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
+import { SurveyModel } from '@/domain/models/survey'
+import { AccountModel } from '@/domain/models/account'
 
 let surveyCollection: Collection
 let surveyResultCollection: Collection
@@ -10,7 +12,7 @@ const makeSut = (): SurveyResultMongoRepository => {
   return new SurveyResultMongoRepository()
 }
 
-const makeSurvey = async () => {
+const makeSurvey = async (): Promise<SurveyModel> => {
   const surveyAtBd = await surveyCollection.insertOne({
     question: 'any_question',
     answers: [{
@@ -22,20 +24,20 @@ const makeSurvey = async () => {
     date: new Date()
   })
 
-  const res = surveyCollection.findOne({ _id: surveyAtBd.insertedId })
+  const res = await surveyCollection.findOne({ _id: surveyAtBd.insertedId })
 
-  return res
+  return MongoHelper.map(res)
 }
 
-const makeAccount = async () => {
+const makeAccount = async (): Promise<AccountModel> => {
   const accountByBd = await accountCollection.insertOne({
     name: 'any_name',
     email: 'any_email@gmail.com',
     password: 'any_password'
   })
 
-  const res = accountCollection.findOne({ _id: accountByBd.insertedId })
-  return res
+  const res = await accountCollection.findOne({ _id: accountByBd.insertedId })
+  return MongoHelper.map(res)
 }
 
 describe('SurveyMongoRepository', () => {
@@ -50,7 +52,7 @@ describe('SurveyMongoRepository', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getColletion('surveys')
     await surveyCollection.deleteMany({})
-    surveyResultCollection = await MongoHelper.getColletion('surveyResults')
+    surveyResultCollection = await MongoHelper.getColletion('surveysResults')
     await surveyResultCollection.deleteMany({})
     accountCollection = await MongoHelper.getColletion('accounts')
     await accountCollection.deleteMany({})
@@ -62,8 +64,8 @@ describe('SurveyMongoRepository', () => {
       const account = await makeAccount()
       const sut = makeSut()
       const surveyResult = await sut.save({
-        surveyId: survey._id.toString(),
-        accountId: account._id.toString(),
+        surveyId: survey.id,
+        accountId: account.id,
         answer: survey.answers[0].answer,
         date: new Date()
       })
@@ -71,6 +73,27 @@ describe('SurveyMongoRepository', () => {
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+    test('Should update a survey result if its not new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(res.insertedId.toHexString())
+      expect(surveyResult.answer).toBe(survey.answers[1].answer)
     })
   })
 })
