51e9456a838d6a0fbad3f25ad2369895bcf61302
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-20 09:28:15 -0300

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,9 +1,10 @@
+import { ok } from '../../../helpers/http/http-helper'
 import { HttpResponse, Controller, HttpRequest, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private loadSurveys : LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
   }
 }
