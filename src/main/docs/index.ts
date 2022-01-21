4ba9b8077b59f82b568d304f615807037f6a9b30
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-21 15:33:15 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,10 +1,36 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node Api',
-    description: 'Api do curso do mango para realizar enquetes entre programadores',
-    version: '1.0.0'
+    description: 'Api do curso do Mango para relatar enquetes entre programadores',
+    version: '1.0.0',
+    contact: {
+      name: 'Felipe Jhordan',
+      email: 'felipejordan.alves@gmail.com',
+      url: 'https://github.com/FelipeJhordan'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
+  },
+  servers: [{
+    url: '/api/v1/',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
-
 }
