542a070aaa2b09fafaef3c36cb7650f61bffb1b8
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-21 14:57:47 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,7 +2,10 @@ import express from 'express'
 import setupMidlewares from './middlewares'
 import setupRoutes from './routes'
 
+import setupSwagger from './config-swagger'
+
 const app = express()
+setupSwagger(app)
 setupMidlewares(app)
 setupRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,10 @@
+
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node Api',
+    description: 'Api do curso do mango para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+
+}
