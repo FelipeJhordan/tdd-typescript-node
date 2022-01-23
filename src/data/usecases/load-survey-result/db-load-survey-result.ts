6bb793e2c437cd36cbb3678bef129aa9ecfa5c34
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-22 22:00:54 -0300

    feat: ensure DbLoadSurveyResultcalls LoadSurveyByIdRepo if LoadSurveyResultRepo returns null

--- src/data/usecases/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.ts
@@ -1,9 +1,16 @@
-import { LoadSurveyResult, LoadSurveyResultRepository, SurveyResultModel } from './db-load-survey-result-protocols'
+import { LoadSurveyByIdRepository, LoadSurveyResult, LoadSurveyResultRepository, SurveyResultModel } from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private loadSurveyResultRepository: LoadSurveyResultRepository,
+    private loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
+
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId)
+    }
     return surveyResult
   }
 }
