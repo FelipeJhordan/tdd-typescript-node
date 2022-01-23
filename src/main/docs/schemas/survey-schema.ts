c6201949a53c670c035812929ed6fdae53980249
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-23 15:13:25 -0300

    docs: update load surveys schema

--- src/main/docs/schemas/survey-schema.ts
+++ src/main/docs/schemas/survey-schema.ts
@@ -15,8 +15,11 @@ export const surveySchema = {
     },
     date: {
       type: 'string'
+    },
+    didAnswer: {
+      type: 'boolean'
     }
 
   },
-  required: ['id', 'question', 'answers', 'date']
+  required: ['id', 'question', 'answers', 'date', 'didAnswer']
 }
