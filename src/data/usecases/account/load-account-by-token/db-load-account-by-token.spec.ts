e328bb211411b04fc4e0e0fb3e5ef80b783d77b8
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-23 15:21:54 -0300

    fix: change DbLoadAccountByToken to return null if Decrypter throws

--- src/data/usecases/account/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token.spec.ts
@@ -60,8 +60,8 @@ describe('DbLoadAccountByToken Usecase', () => {
   test('Should throw if Decrypter throws', async () => {
     const { sut, decrypterStub } = makeSut()
     jest.spyOn(decrypterStub, 'decrypt').mockImplementationOnce(throwError)
-    const promise = sut.load('any_token', 'any_role')
-    await expect(promise).rejects.toThrow()
+    const account = await sut.load('any_token', 'any_role')
+    await expect(account).toBeNull()
   })
   test('Should throw if LoadAccountByTokenRepository throws', async () => {
     const { sut, loadAccountByTokenRepositoryStub } = makeSut()
--- src/data/usecases/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token.ts
@@ -10,7 +10,12 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   ) {}
 
   async load (accessToken: string, role?: string): Promise<AccountModel> {
-    const token = await this.decrypter.decrypt(accessToken)
+    let token: string
+    try {
+      token = await this.decrypter.decrypt(accessToken)
+    } catch (e) {
+      return null
+    }
     if (token) {
       const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
